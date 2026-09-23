3d5a9d2ecfb0e5f68025ed97bb64386a91f5c942
unicobardo-coder <unicobardo@gmail.com>
2026-09-23 10:54:53 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const LS3D_CONFIG = {
+    emailjs: {
+        publicKey: "IL_TUO_PUBLIC_KEY",          // La tua chiave pubblica generale
+        serviceId: "IL_TUO_SERVICE_ID",          // Il tuo Service ID
+        orderTemplateId: "template_xxxxxxx",     // <-- Inserisci qui l'ID del template per gli ORDINI
+        contactTemplateId: "template_yyyyyyy"    // <-- Inserisci qui l'ID del template per i CONTATTI/INFO
+    },
+    paypal: {
+        clientId: "IL_TUO_PAYPAL_CLIENT_ID"
+    },
+    stripe: {
+        publicKey: "pk_test_..."
+    }
+};
